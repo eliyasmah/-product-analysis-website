19a158589ea0b23e8b3e8d0ecf880281c665ddff
eliyas <eliyasbubt@gmail.com>
2022-04-04 02:39:15 +0600

    home page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
+import HomePage from './HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+      </Routes>
     </div>
   );
 }
