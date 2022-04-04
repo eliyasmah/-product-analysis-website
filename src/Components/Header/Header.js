b8c8763be33279c66a6865b090ff509d38046084
eliyas <eliyasbubt@gmail.com>
2022-04-04 16:56:34 +0600

    custom-link added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <div className='text-xl flex gap-4 justify-center text-center bg-slate-300 py-5'>
-            <Link to="/">HOME</Link>
-            <Link to="/review">REVIEWS</Link>
-            <Link to="/dashboard">DASHBOARD</Link>
-            <Link to="/blogs">BLOGS</Link>
+            <CustomLink to="/">HOME</CustomLink>
+            <CustomLink to="/review">REVIEWS</CustomLink>
+            <CustomLink to="/dashboard">DASHBOARD</CustomLink>
+            <CustomLink to="/blogs">BLOGS</CustomLink>
         </div>
     );
 };
