28096db9c2c3ea75dbe168f6907d13c77be0b83b
eliyas <eliyasbubt@gmail.com>
2022-04-05 03:40:41 +0600

    review page design done

--- src/Components/Carts/Carts.js
+++ src/Components/Carts/Carts.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import "./Carts.css";
 
 const Carts = (props) => {
-    const { name, img, rating } = props.cart;
+    const { name, img, rating, review } = props.cart;
     return (
-        <div>
-            <h1>this is bcart</h1>
-            <img src={img} alt="" />
+        <div className='bg-slate-100 p-5 rounded-md home-reviews'>
+            <img className='h-24 w-24 rounded-full m-auto' src={img} alt=" " />
+            <h1 className='text-xl text-center py-3 text-slate-600'>{name}</h1>
+            <p>{review}</p>
+            <button className='bg-slate-700 py-3 rounded-md text-white home-reviews-btn'>Rating: {rating}</button>
         </div>
     );
 };
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -6,7 +6,7 @@ const Reviews = () => {
     const [carts, setCarts] = useData();
 
     return (
-        <div>
+        <div className='md:grid grid-cols-3 gap-10 mx-20 mt-16'>
             {
                 carts.map(cart => <Carts
                     key={cart._id}
